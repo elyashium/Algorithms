951f6d1ee7df0fdd268dd62a3033d3d3207b74b4
shash <146242897+elyashium@users.noreply.github.com>
2024-10-05 11:27:11 +0530

    Update infixTOpostfix.java

--- stack and Queues/prefix_infix_postfix/infixTOpostfix.java
+++ stack and Queues/prefix_infix_postfix/infixTOpostfix.java
@@ -82,3 +82,47 @@ class infixTOpostfix {
         System.out.println("Prefix expression: " + infixToPostfix(exp));
     }
 }
+
+
+
+//GFG SOLUTION 
+
+
+
+
+class Solution {
+    // Function to convert an infix expression to a postfix expression.
+    public static String infixToPostfix(String exp) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<exp.length();i++){
+            char ch = exp.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(ch);
+            }
+            else if(ch=='(') st.push(ch);
+            else if(ch==')'){
+                while(!st.isEmpty() && st.peek()!='('){
+                    sb.append(st.pop());
+                }
+            st.pop();
+            }
+            else{
+                while(!st.isEmpty() && precedence(ch)<=precedence(st.peek())){
+                    sb.append(st.pop());
+                }
+            st.push(ch);
+            }
+        }
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+    return sb.toString();
+    }
+    public static int precedence(char ch){
+        if (ch=='^') return 3;
+        else if(ch=='*' || ch=='/') return 2;
+        else if(ch=='+' || ch=='-') return 1;
+        else return -1;
+    }
+}
