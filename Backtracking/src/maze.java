d837a27587487a09d66ab53b3fd65322af269cf0
shash <146242897+elyashium@users.noreply.github.com>
2024-10-09 00:24:05 +0530

    Update maze.java

--- Backtracking/src/maze.java
+++ Backtracking/src/maze.java
@@ -8,7 +8,6 @@ public class maze {
 //        path("", 3, 3);
 //        System.out.println(pathRet("", 3, 3));
 //        System.out.println(pathRetDiagonal("", 3, 3));
-        //
         
         boolean[][] board = {
                 {true, true, true},
