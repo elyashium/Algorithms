f4d02992783f994f4d5a8a5bb2dc09c38c832cd7
shash <146242897+elyashium@users.noreply.github.com>
2024-11-23 20:38:28 +0530

    Add files via upload

---
+++ Trees/BT_Representation.java
@@ -0,0 +1,16 @@
+public class BT_Representation {
+
+    // Node class to represent each node in the binary tree
+    class Node {
+        int data;
+        Node left;
+        Node right;
+
+        // Constructor for creating a new node
+        public Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+}
