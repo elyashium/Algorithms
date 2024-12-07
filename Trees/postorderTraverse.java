02c318778118cbf6e38e7e7d5cb7eef725453b18
Ashish <146242897+elyashium@users.noreply.github.com>
2024-12-07 19:01:57 +0530

    Update postorderTraverse.java

--- Trees/postorderTraverse.java
+++ Trees/postorderTraverse.java
@@ -13,3 +13,32 @@ class Solution {
         return res;
     }
 }
+
+
+//iterative approach using 2 stack.
+
+class solution{
+    public List<Integer> postOrderTraversal(TreeNode root){
+        Stack<Treenode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        ArrayList<Integer> ans = new List<>();
+        
+        if(root == null) return ans;
+        
+        st1.push(root);
+        while(!st1.isEmpty()){
+            root = st1.pop();
+            st2.add(root);
+            
+            if(root.left != null) st1.push(root.left);
+            if(root.right != null) st1.push(root.right);
+            
+        }
+        
+        while(!st2.isEmpty()){
+            ans.add(st2.pop().val);
+        }
+        
+        return ans;
+    }
+}
