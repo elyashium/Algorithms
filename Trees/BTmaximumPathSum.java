ef95d4eb518c9fb21a60726a6bd29c8ff13b8ab9
Ashish <146242897+elyashium@users.noreply.github.com>
2024-12-13 19:14:44 +0530

    Add files via upload

---
+++ Trees/BTmaximumPathSum.java
@@ -0,0 +1,20 @@
+public class BTmaximumPathSum {
+
+    int res = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int leftSum = Math.max(0, dfs(root.left)); // to deal with negative number
+        int rightSum = Math.max(0, dfs(root.right));
+
+        res = Math.max(res, leftSum + rightSum + root.val);
+        return (root.val) + Math.max(leftSum, rightSum);
+    }
+}
