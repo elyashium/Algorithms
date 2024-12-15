dca5dd7b9855936d26a067f572c749a2e05aabe2
Ashish <146242897+elyashium@users.noreply.github.com>
2024-12-15 23:01:22 +0530

    Update BTmaximumPathSum.java

--- Trees/BTmaximumPathSum.java
+++ Trees/BTmaximumPathSum.java
@@ -11,7 +11,7 @@ public class BTmaximumPathSum {
         if (root == null)
             return 0;
 
-        int leftSum = Math.max(0, dfs(root.left)); // to deal with negative number
+        int leftSum = Math.max(0, dfs(root.left)); // math.max to deal with negative number
         int rightSum = Math.max(0, dfs(root.right));
 
         res = Math.max(res, leftSum + rightSum + root.val);
