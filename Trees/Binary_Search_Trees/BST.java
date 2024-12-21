148322e8aa8a2c9a2e422e106a55cb0a98d9fb0f
Ashish <146242897+elyashium@users.noreply.github.com>
2024-12-21 23:11:00 +0530

    Add files via upload

---
+++ Trees/Binary_Search_Trees/BST.java
@@ -0,0 +1,18 @@
+package Binary_Search_Trees;
+
+public class BST {
+
+        public TreeNode searchBST(TreeNode root, int val) {
+            if (root == null) {
+                return null;
+            }
+            if (root.val == val) {
+                return root;
+            } else if (root.val > val) {
+                return searchBST(root.left, val);
+            } else {
+                return searchBST(root.right, val);
+            }
+        }
+    }
+
