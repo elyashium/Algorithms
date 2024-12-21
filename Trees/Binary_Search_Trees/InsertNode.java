61849bf23d8e0b24ef726ae7d6a9bc348d5c491b
Ashish <146242897+elyashium@users.noreply.github.com>
2024-12-21 23:20:40 +0530

    Update InsertNode.java

--- Trees/Binary_Search_Trees/InsertNode.java
+++ Trees/Binary_Search_Trees/InsertNode.java
@@ -1,4 +1,20 @@
 package Binary_Search_Trees;
 
 public class InsertNode {
-}
+    
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+            if(root==null){
+                root = new TreeNode(val);
+                return root;
+            }
+            if(val<root.val){
+                root.left = insertIntoBST(root.left, val);
+            }
+            else{
+                root.right = insertIntoBST(root.right, val);
+            }
+            return root;
+
+        }
+    }
+
