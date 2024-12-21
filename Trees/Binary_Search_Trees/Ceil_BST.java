513e3a2bdfc87d8cec004e66d8997c6ec3a251b1
Ashish <146242897+elyashium@users.noreply.github.com>
2024-12-21 23:46:43 +0530

    Update Ceil_BST.java

--- Trees/Binary_Search_Trees/Ceil_BST.java
+++ Trees/Binary_Search_Trees/Ceil_BST.java
@@ -1,4 +1,32 @@
 package Binary_Search_Trees;
 
 public class Ceil_BST {
+
+   private static int ceil;
+
+    int findCeil(Node root, int key) {
+        if (root == null) return -1;
+
+        ceil = -1; // Reset ceil for each call
+        search(root, key); 
+        return ceil;
+    }
+
+    private static void search(Node node, int value) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.data == value) {
+            ceil = node.data; 
+            return;
+        }
+
+        if (node.data > value) {
+            ceil = node.data; // Update ceil
+            search(node.left, value); 
+        } else {
+            search(node.right, value); // No need to update ceil, search in the right subtree
+        }
+    }
 }
