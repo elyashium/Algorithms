3118f2d9267c98e6dd780862a56bd1e9f0a57917
Ashish <146242897+elyashium@users.noreply.github.com>
2024-12-28 18:58:11 +0530

    Update LowestCommonAncestor.java

--- Trees/LowestCommonAncestor.java
+++ Trees/LowestCommonAncestor.java
@@ -1,2 +1,28 @@
 public class LowestCommonAncestor {
-}
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root == null || p == root || q == root){
+                return root;
+            }
+
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+            //if we get both sides null, that means there is no common ancestor.
+
+            if(left == null){
+                return right;
+            }
+            else if(right == null){
+                return left;
+            }
+
+            //if both are not null, then that means the nodes that came back are under
+            //current node.
+
+            else{
+                return root;
+            }
+
+        }
+    }
