9ddd461a6c8adb4520117539ad1a1112e8c25ecb
Ashish <146242897+elyashium@users.noreply.github.com>
2025-01-07 22:58:59 +0530

    Update Maximum_Width.java

--- Trees/Maximum_Width.java
+++ Trees/Maximum_Width.java
@@ -1,2 +1,23 @@
-public class Maximum_Width {
+class Solution {
+
+    class Pair{
+        TreeNode node;
+        int num;
+        Pair(TreeNode _node, int _num){
+            node = _num;
+            num = _num;
+        }
+    }
+
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null ) return;
+        Queue<Pair> q = new LinkedList()<>;
+        q.offer(new Pair(root,0));
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            int min = q.peek.num; 
+
+        }
+    }
 }
