d3d75ba1fb7fb26bb1383fabdf38d19ba30e29cc
Shash <ashishsinghlogic@gmail.com>
2025-05-30 23:33:48 +0530

    Update findMinimumInsortedArrayII.java

--- Binary Search/findMinimumInsortedArrayII.java
+++ Binary Search/findMinimumInsortedArrayII.java
@@ -1,3 +1,24 @@
 package Binary Search;
 
 
+class Solution {
+    public int findMin(int[] nums) {
+        int s = 0;
+        int e = nums.length - 1;
+
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == nums[s] && nums[mid] == nums[e]) {
+                s++;
+                e--;
+                //additional check to shrink the window where we are checking things
+            } else if (nums[mid] > nums[e]) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+        return nums[s];
+
+    }
+}
