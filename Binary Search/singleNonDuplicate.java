21f638e4ba9f16b25e7b2f99d70ab33edbe9cd29
Shash <ashishsinghlogic@gmail.com>
2025-05-30 22:47:44 +0530

    Create singleNonDuplicate.java

---
+++ Binary Search/singleNonDuplicate.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int s =0;
+        int e = nums.length-1;
+        while (s < e) {
+            int mid = s+(e-s)/2;
+            if (mid % 2 == 1){
+                 mid--;
+                 }
+            if (nums[mid] == nums[mid + 1]) {
+                s = mid + 2;
+            } else {
+                e = mid;
+            }
+        }
+        return nums[s];
+    }
+}
