b27d0fa4a554c89de744df43eae78f553f083b0d
Ashish <146242897+elyashium@users.noreply.github.com>
2025-06-23 17:11:19 +0530

    Update removeDuplicates.java

--- LinkedList/removeDuplicates.java
+++ LinkedList/removeDuplicates.java
@@ -15,6 +15,7 @@ class Solution {
         while(head != null && head.next != null){
             if(head.val  == head.next.val){
                 head.next = head.next.next;
+                //skipping the duplicate node link
             }
             else{
                 head = head.next;
