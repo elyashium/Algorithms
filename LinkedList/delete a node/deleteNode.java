272bd10fc582b52929d836165178acbca2c5f87c
Shash <ashishsinghlogic@gmail.com>
2025-08-03 16:46:23 +0530

    Update deleteNode.java

--- LinkedList/delete a node/deleteNode.java
+++ LinkedList/delete a node/deleteNode.java
@@ -1,5 +1,19 @@
-package LinkedList.delete a node;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
 
-public class deleteNode {
-    
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+       
+        
+
+    }
 }
