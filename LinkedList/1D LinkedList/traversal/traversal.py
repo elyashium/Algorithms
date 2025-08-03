211a5c087f95042e08cda3672c6d94a0f9dee6f3
Shash <ashishsinghlogic@gmail.com>
2025-08-03 17:23:37 +0530

    Add getLength method for linked list traversal
    Replaced insertAtHead with getLength in both Java and Python traversal implementations. The new method counts and returns the number of nodes in a singly linked list.

--- LinkedList/1D LinkedList/traversal/traversal.py
+++ LinkedList/1D LinkedList/traversal/traversal.py
@@ -1,13 +1,10 @@
-# Definiton of singly Linked List
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
-    def insertAtHead(self, head, X):
-        temp = ListNode(X)
-        temp.next = head
-        return temp
+    def getLength(self, head):
+        count = 0
+        temp = head
 
-        
+        while temp is not None:
+            count += 1
+            temp = temp.next
+
+        return count
