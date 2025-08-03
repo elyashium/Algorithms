69e2602e77168f2265980dfc3a9df2cd7022d541
Shash <ashishsinghlogic@gmail.com>
2025-08-03 17:05:23 +0530

    Update insertNode.ts

--- LinkedList/insert a node (atHead)/insertNode.ts
+++ LinkedList/insert a node (atHead)/insertNode.ts
@@ -1,21 +1,18 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+/*Definiton of singly Linked List
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+*/
 
-/**
- Do not return anything, modify it in-place instead.
- */
-function deleteNode(node: ListNode | null): void {
-    node.val = node.next.val;
-    node.next = node.next.next;
+class Solution {
+    insertAtHead(head, X) {
 
+        let temp = ListNode(X);
+        temp.next = head;
+        return temp;
 
-};
+    }
+}
