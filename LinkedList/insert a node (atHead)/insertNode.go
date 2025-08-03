5e66a4788e7743e5783232985fe59da1493beb3f
Shash <ashishsinghlogic@gmail.com>
2025-08-03 17:11:52 +0530

    Update insertNode.go

--- LinkedList/insert a node (atHead)/insertNode.go
+++ LinkedList/insert a node (atHead)/insertNode.go
@@ -5,7 +5,15 @@
  *     Next *ListNode
  * }
  */
-func deleteNode(node *ListNode) {
-    node.Val = node.Next.Val;
-    node.Next = node.Next.Next;
+
+func insertAtHead(head *ListNode, X int) *ListNode {
+	return &ListNode{Val: X, Next: head}
+
+	//this is how you intilaise a struct in go, it is equivalent to
+
+	/*
+	    var temp *ListNode          // declaration only
+	temp = &ListNode{Val: X}    // assigned later
+
+	*/
 }
