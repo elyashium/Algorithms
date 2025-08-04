7c6c68095c1212ec3d5054415f089f51a9c55696
Shash <ashishsinghlogic@gmail.com>
2025-08-04 18:22:03 +0530

    Update reverse.go

--- LinkedList/1D LinkedList/Reverse Between/reverse.go
+++ LinkedList/1D LinkedList/Reverse Between/reverse.go
@@ -1,27 +1,30 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
+
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
+    if head == nil || left == right {
+        return head
+    }
 
-	  if (head == null || left == right) {
-            return head;
-        }
-    
-	var temp *ListNode = &ListNode{Val:0, Next:head}
-	var Dummy *ListNode = &ListNode{val:temp}
-	//going back one node behind
-	int i =0;
-	for i<left-1{
-		prev = prev.Next;
-	}
+    dummy := &ListNode{Val: 0, Next: head}
+    prev := dummy
 
-	var curr ListNode* = &ListNode{Val:prev.next}
-	for right-left{
-		
-	}
+    // Move prev one node before left
+    for i := 0; i < left-1; i++ {
+        prev = prev.Next
+    }
 
+    cur := prev.Next
+
+    // In-place reversal of sublist
+    for i := 0; i < right-left; i++ {
+        temp := cur.Next
+        cur.Next = temp.Next
+        temp.Next = prev.Next
+        prev.Next = temp
+    }
+
+    return dummy.Next
 }
