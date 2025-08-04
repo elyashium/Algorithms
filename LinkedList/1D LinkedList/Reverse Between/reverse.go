d4425cd5022025ffcc35bd0a90d4507210c51fbe
Shash <ashishsinghlogic@gmail.com>
2025-08-04 18:21:38 +0530

    Refactor and implement reverseBetween in Go and Java
    Refactored the reverseBetween function for singly-linked lists in both Go and Java. The Go implementation was started and partially rewritten, while the Java implementation was replaced with a Go version, including the ListNode struct and the in-place sublist reversal logic.

--- LinkedList/1D LinkedList/Reverse Between/reverse.go
+++ LinkedList/1D LinkedList/Reverse Between/reverse.go
@@ -6,5 +6,22 @@
  * }
  */
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
+
+	  if (head == null || left == right) {
+            return head;
+        }
     
+	var temp *ListNode = &ListNode{Val:0, Next:head}
+	var Dummy *ListNode = &ListNode{val:temp}
+	//going back one node behind
+	int i =0;
+	for i<left-1{
+		prev = prev.Next;
+	}
+
+	var curr ListNode* = &ListNode{Val:prev.next}
+	for right-left{
+		
+	}
+
 }
--- LinkedList/1D LinkedList/Reverse Between/reverse.java
+++ LinkedList/1D LinkedList/Reverse Between/reverse.java
@@ -1,36 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || left == right) {
-            return head;
-        }
+type ListNode struct {
+    Val  int
+    Next *ListNode
+}
 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-
-        for (int i = 0; i < left - 1; i++) {
-            prev = prev.next;
-        }
-
-        ListNode cur = prev.next;
-
-        for (int i = 0; i < right - left; i++) {
-            ListNode temp = cur.next;
-            cur.next = temp.next;
-            temp.next = prev.next;
-            prev.next = temp;
-        }
-
-        return dummy.next;        
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+    if head == nil || left == right {
+        return head
     }
+
+    dummy := &ListNode{Val: 0, Next: head}
+    prev := dummy
+
+    // Move prev one node before left
+    for i := 0; i < left-1; i++ {
+        prev = prev.Next
+    }
+
+    cur := prev.Next
+
+    // In-place reversal of sublist
+    for i := 0; i < right-left; i++ {
+        temp := cur.Next
+        cur.Next = temp.Next
+        temp.Next = prev.Next
+        prev.Next = temp
+    }
+
+    return dummy.Next
 }
