d9d1cd46d82b9647bce57a6a11c78ace7bbfcb61
Shash <ashishsinghlogic@gmail.com>
2025-08-04 17:49:52 +0530

    Update reverse.py

---
+++ LinkedList/1D LinkedList/Reverse (iterative)/reverse.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        previous = None
+        current = head
+
+        while current:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+
+        return previous
