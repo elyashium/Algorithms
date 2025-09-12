99e10e30a55dfb1554153b1dc3c2fcdf985a794e
Shash <ashishsinghlogic@gmail.com>
2025-09-12 22:27:46 +0530

    Update RotateString.ts

--- Strings/RotateString/RotateString.ts
+++ Strings/RotateString/RotateString.ts
@@ -1,19 +1,9 @@
-import java.util*;
-class solution{
-    public boolean rotate(String s, String goal){
-
-        int n = s.length();
-        int m = s.length();
-
-        if(m!=n){return false;}
-
-        for(int i =0; i<m; i++){
-            if(s.toCharArray(i) == goal.toCharArray(i)){
-                return true;
-            }
-            else{return false;}
+class Solution {
+    public boolean rotate(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
         }
-        
-
+        String doubled = s + s;
+        return doubled.contains(goal);
     }
 }
