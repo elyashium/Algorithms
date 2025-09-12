6429e021c33fa9204ce60725b4976e21023811d7
Shash <ashishsinghlogic@gmail.com>
2025-09-12 22:52:27 +0530

    Update iso.py

--- Strings/isomorphic/iso.py
+++ Strings/isomorphic/iso.py
@@ -1,11 +1,28 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        #you cant use sort directely becasuse strings in python are immuatable
-        #sorted(s) → which returns a list of sorted characters.
-        return sorted(s) == sorted(t)
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map_s, map_t = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+        for cs, ct in zip(s, t):
+            if cs in map_s:
+                if map_s[cs] != ct:
+                    return False
+            else:
+                map_s[cs] = ct
+
+            if ct in map_t:
+                if map_t[ct] != cs:
+                    return False
+            else:
+                map_t[ct] = cs
+
+        return True
 
 
 # Example
 sol = Solution()
-print(sol.isAnagram("anagram", "nagaram"))  # True
-print(sol.isAnagram("rat", "car"))          # False
+print(sol.isIsomorphic("egg", "add"))  # True
+print(sol.isIsomorphic("foo", "bar"))  # False
+print(sol.isIsomorphic("paper", "title"))  # True
