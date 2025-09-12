3faa5d2693eb7759f452a2e2067d47aea33806a5
Shash <ashishsinghlogic@gmail.com>
2025-09-12 22:12:10 +0530

    Update RemoveOuterMostParanthesis.java

--- Strings/RemoveOutermostParanthesis/RemoveOuterMostParanthesis.java
+++ Strings/RemoveOutermostParanthesis/RemoveOuterMostParanthesis.java
@@ -1,5 +1,64 @@
-//stack based solution 
 
-class solution{
-    public static void RemoveOuterMostParanthesis()
+//stack based solution 
+import java.util.*;
+
+class solution {
+    public static void RemoveOuterMostParanthesis(String s){
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch: s.toCharArray()){
+            if(ch == "("){
+                //if stack is not empty then its not a empty stack
+                if(!stack.isEmpty()){
+                    sb.append(ch);
+                }
+                stack.push(ch);
+                else{
+                    stack.pop();
+                     // If stack is not empty after popping, this ')' is not an outermost one
+                     if(!stack.isEmpty()){
+                        sb.append(ch);
+                     }
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
+
+// without stack keeping track of the counters
+
+class solution {
+    public static void RemoveOuterMostParanthesis(String s) {
+        int count = 0;
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        if (n < 2) {
+            return "";
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (c[i] == '(') {
+                if (x > 0) {
+                    sb.append('(');
+                }
+                x++;
+            }
+
+            else {
+                x--;
+
+                if (x > 0) {
+                    sb.append(')');
+                }
+
+            }
+        }
+
+        return sb.toString();
+    }
 }
