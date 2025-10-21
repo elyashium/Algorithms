98a416a4d066a3b7300961d93a43232471bad221
Shash <ashishsinghlogic@gmail.com>
2025-10-21 06:31:24 +0530

    Update selection.java

--- Sorting/selection/selection.java
+++ Sorting/selection/selection.java
@@ -25,6 +25,21 @@ class solution{
     }
 
     public static void selectionSort(int[] nums){
-        
+         int n = nums.length;
+
+        // One by one move the boundary of the unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in the unsorted array [i...n-1]
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j; // Found a new minimum
+                }
+            }
+
+            // Swap the found minimum element with the first element of the unsorted part
+            int temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = temp;
     }
 }
