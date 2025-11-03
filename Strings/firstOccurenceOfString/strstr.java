7a8787402182d73a91da8e07bd0871f6592aa7b8
Shash <ashishsinghlogic@gmail.com>
2025-11-04 00:05:36 +0530

    Create strstr.java

---
+++ Strings/firstOccurenceOfString/strstr.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string and the substrnig");
+        String str = sc.nextLine();
+        String substr = sc.nextLine();
+
+        System.out.println("the first occurence is at index " + strStr(str, substr));
+        sc.close();
+
+    }
+
+    
+    public int strStr(String haystack, String needle) {
+
+        // we have to find the first time needle is found in haystack
+        // and return the index. and -1 if its not found.
+        // its given the that needle and haystack only contain lowercase
+
+        if (needle.isEmpty())
+            return 0;
+        if (needle.length() > haystack.length())
+            return -1;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
